d0cd27431854dd9cbc5bda8d512ba3a675fda353
Hamish MacDonald <elusivenode@gmail.com>
2021-12-25 11:10:36 +1000

    Working on ch1 q1.

--- src/chapter1.c
+++ src/chapter1.c
@@ -1,16 +1,40 @@
 #include <stdio.h>
 
 int mainMenu();
+void q1();
 
 int main() {
 
     int userSelection;
     do {
         userSelection = mainMenu();
+        switch (userSelection) {
+            case 1:
+                q1();
+        }
     } while (userSelection != 0);
 
     printf("Goodbye.\n");
     return 0;
+}
+
+void q1() {
+    int noInts = 10;
+    int ints[noInts];
+    for (int i = 0; i < noInts; i++) {
+        printf("%\tEnter integer number %d\n", i+1);
+        scanf("%d", &ints[i]);
+    }
+
+    unsigned char delimit;
+    for (int i = 0; i < noInts; i++) {
+        if ((i + 1) % 3 == 0) || (i + 1) == noInts ) {
+            delimit= 0x0a;
+        } else {
+            delimit= 0x2c;
+        }
+        printf("%d%c", ints[i], delimit);
+    }
 }
 
 int mainMenu() {
@@ -18,6 +42,7 @@ int mainMenu() {
 
     printf("Select a question number:\n");
     printf("0:\tQUIT\n");
+    printf("1:\tPrint integers\n");
     scanf("%d", &userSelection);
     return userSelection;
 }
