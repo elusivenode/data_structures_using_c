f1c25a932e1e2cec27cc8dd29e4618100a4f7d1e
Hamish MacDonald <elusivenode@gmail.com>
2021-12-27 17:46:44 +1000

    Chapter 1 qu3.

--- src/chapter1.c
+++ src/chapter1.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 
 int mainMenu();
-void q1(), q2();
+void q1(), q2(), q3();
 
 int main() {
 
@@ -15,6 +18,9 @@ int main() {
             case 2:
                 q2();
                 break;
+            case 3:
+                q3();
+                break;
             default:
                 printf("You must enter a number from the menu.\n");
         }
@@ -24,10 +30,69 @@ int main() {
     return 0;
 }
 
+void q3() {
+    printf("\n**********************************************\n");
+    printf("Question 3\n");
+    printf("**********************************************\n\n");
+
+
+    unsigned int len_max = 128;
+    unsigned int current_size = 0;
+
+    char *pStr = malloc(len_max);
+    current_size = len_max;
+
+    printf("\nPlease enter one line of text ended with a } (can be of arbitrary length): ");
+
+    if(pStr != NULL)
+    {
+        int c;
+        unsigned int i = 0;
+        //accept user input until hit enter or end of file
+        do {
+            c = getchar();
+            pStr[i++]=c;
+
+            //if i reached maximize size then realloc size
+            if(i == current_size)
+            {
+                current_size = i+len_max;
+                pStr = realloc(pStr, current_size);
+            }
+        } while (c != '}' && c != EOF);
+
+        pStr[i-1] = '\0';
+
+        int vowels = 0;
+        for (i = 0; i < strlen(pStr); i++) {
+            switch (pStr[i]) {
+                case 'A':
+                case 'a':
+                case 'E':
+                case 'e':
+                case 'I':
+                case 'i':
+                case 'O':
+                case 'o':
+                case 'U':
+                case 'u':
+                    vowels++;
+            }
+        }
+        printf("\nThe string entered was: %s\n",pStr);
+        printf("This string contains %d vowels.\n", vowels);
+        //free the memory
+        free(pStr);
+        pStr = NULL;
+    }
+
+    printf("\n\n");
+}
+
 void q2() {
     printf("\n**********************************************\n");
     printf("Question 2\n");
-    printf("**********************************************\n");
+    printf("**********************************************\n\n");
 
     int userInt;
     printf("Please enter an integer number:");
@@ -46,7 +111,7 @@ void q2() {
 void q1() {
     printf("\n**********************************************\n");
     printf("Question 1\n");
-    printf("**********************************************\n");
+    printf("**********************************************\n\n");
 
     int noInts = 10;
     int ints[noInts];
@@ -74,6 +139,7 @@ int mainMenu() {
     printf("0:\tQUIT\n");
     printf("1:\tPrint integers\n");
     printf("2:\tSum even numbers between 1 and x\n");
+    printf("3:\tCount the number of vowels\n");
     scanf("%d", &userSelection);
     return userSelection;
 }
